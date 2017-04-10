122920d7c2a9c29a84748a7ea1c41314fc8a7c01
Lenovo-PC\thomasw <kontakt@themecoder.de>
2017-04-10 09:15:31 +0200

    fixed horizontal scrolling issue when elements are hidden on mobile devices

--- inc/extras.php
+++ inc/extras.php
@@ -106,7 +106,7 @@ function tortuga_hide_elements() {
 
 	// Create CSS.
 	$classes = implode( ', ', $elements );
-	$custom_css = $classes . ' { position: absolute; clip: rect(1px, 1px, 1px, 1px); }';
+	$custom_css = $classes . ' { position: absolute; clip: rect(1px, 1px, 1px, 1px); width: 1px; height: 1px; overflow: hidden; }';
 
 	// Add Custom CSS.
 	wp_add_inline_style( 'tortuga-stylesheet', $custom_css );
