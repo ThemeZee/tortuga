61e141dc473e979c1702216085e7f2482fcb6b6a
Thomas W <kontakt@themecoder.de>
2018-04-13 15:50:43 +0200

    added local fonts

--- functions.php
+++ functions.php
@@ -73,7 +73,7 @@ if ( ! function_exists( 'tortuga_setup' ) ) :
 		add_theme_support( 'woocommerce' );
 
 		// Add extra theme styling to the visual editor.
-		add_editor_style( array( 'css/editor-style.css', tortuga_google_fonts_url() ) );
+		add_editor_style( array( 'css/editor-style.css', get_template_directory_uri() . '/assets/css/custom-fonts.css' ) );
 
 		// Add Theme Support for Selective Refresh in Customizer.
 		add_theme_support( 'customize-selective-refresh-widgets' );
@@ -157,9 +157,6 @@ function tortuga_scripts() {
 	// Register and enqueue navigation.js.
 	wp_enqueue_script( 'tortuga-jquery-navigation', get_template_directory_uri() . '/js/navigation.js', array( 'jquery' ), '20160719' );
 
-	// Register and Enqueue Google Fonts.
-	wp_enqueue_style( 'tortuga-default-fonts', tortuga_google_fonts_url(), array(), null );
-
 	// Register Comment Reply Script for Threaded Comments.
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
@@ -170,22 +167,15 @@ add_action( 'wp_enqueue_scripts', 'tortuga_scripts' );
 
 
 /**
- * Retrieve Font URL to register default Google Fonts
+ * Enqueue custom fonts.
  */
-function tortuga_google_fonts_url() {
+function tortuga_custom_fonts() {
 
-	// Set default Fonts.
-	$font_families = array( 'Open Sans:400,400italic,700,700italic', 'Titillium Web:400,400italic,700,700italic' );
+	// Register and Enqueue Theme Fonts.
+	wp_enqueue_style( 'tortuga-custom-fonts', get_template_directory_uri() . '/assets/css/custom-fonts.css', array(), '20180413' );
 
-	// Build Fonts URL.
-	$query_args = array(
-		'family' => urlencode( implode( '|', $font_families ) ),
-		'subset' => urlencode( 'latin,latin-ext' ),
-	);
-	$fonts_url = add_query_arg( $query_args, '//fonts.googleapis.com/css' );
-
-	return apply_filters( 'tortuga_google_fonts_url', $fonts_url );
 }
+add_action( 'wp_enqueue_scripts', 'tortuga_custom_fonts', 1 );
 
 
 /**
