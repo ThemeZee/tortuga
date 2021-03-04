b1624490824898c7c3bf78d0ca192df07889b03e
Thomas Weichselbaumer <kontakt@themecoder.de>
2021-03-04 10:38:20 +0100

    updated CSS variables

--- inc/slider.php
+++ inc/slider.php
@@ -28,7 +28,7 @@ function tortuga_slider_scripts() {
 		wp_enqueue_script( 'tortuga-slider', get_template_directory_uri() . '/assets/js/slider.js', array( 'jquery-flexslider' ), '20170421' );
 
 		// Register and enqueue slider CSS.
-		wp_enqueue_style( 'tortuga-slider', get_template_directory_uri() . '/assets/css/flexslider.css', array(), '20170421' );
+		wp_enqueue_style( 'tortuga-slider', get_template_directory_uri() . '/assets/css/flexslider.css', array(), '20210304' );
 
 	endif;
 
