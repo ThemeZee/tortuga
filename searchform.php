b1d293252658b805206cbce095dcb46a53c325cf
Thomas Weichselbaumer <kontakt@themecoder.de>
2021-05-18 20:03:20 +0200

    added SVG icon for search form

--- searchform.php
+++ searchform.php
@@ -2,6 +2,7 @@
 /**
  * Custom Markup for Search form
  *
+ * @version 1.1
  * @package Tortuga
  */
 
@@ -16,7 +17,7 @@
 			title="<?php echo esc_attr_x( 'Search for:', 'label', 'tortuga' ); ?>" />
 	</label>
 	<button type="submit" class="search-submit">
-		<span class="genericon-search"></span>
+		<?php echo tortuga_get_svg( 'search' ); ?>
 		<span class="screen-reader-text"><?php echo esc_html_x( 'Search', 'submit button', 'tortuga' ); ?></span>
 	</button>
 </form>
